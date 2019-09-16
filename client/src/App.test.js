ae92327fd1d2eba629622deab943296c60950509
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-09-15 23:43:39 -0400

    created test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// test("Render players county that from Italy ", async () => {
+//   expect.assertion(3);
+//   const data = await functions.fetchUser();
+//   expect(data.country).toEqual("Italy");
+// });
+describe("App", () => {
+  it('renders "all about Italy players" text', () => {
+    const wrapper = rtl.render(<App />);
+    const players = wrapper.queryByText(/Italy/i);
+  });
+});
+
+describe("App", () => {
+  it("find word WorldCup", () => {
+    const simulateDom = rtl.render(<App />);
+    const h1 = simulateDom.queryByText(/WorldCup/i);
+    console.log(h1.textContent);
+  });
+});
+
+describe("App", () => {
+  it("find the wordfetch ", () => {
+    const simulateDom = rtl.render(<App />);
+    const h1 = simulateDom.queryByText(/fetch/i);
+    console.log(h1.textContent);
+  });
 });
