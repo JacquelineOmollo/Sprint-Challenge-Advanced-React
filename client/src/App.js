0e53142e00b86a34563a2485a1e06d5683245756
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-09-15 21:45:38 -0400

    first commit set up and render data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:5000/api/players`)
+    .then(res => res.json())
+      .then(country => this.setState({ data:country }))
+      .catch(err => console.log("noooo"));
+  }
+
+  render() {
+  console.log(this.state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <h1>Women's World Cup</h1>
+      {this.state.data.map(women => (
+        <div>
+          <p>Name: {women.name}</p>
+          <p>Country: {women.country}</p>
+        </div>
+      ))}
     </div>
   );
+}
 }
 
 export default App;
